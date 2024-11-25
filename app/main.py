451ffc966ddb444c021c15e9f8f0171c75c85cac
Oluwatobi <tobiolajide01@gmail.com>
2024-11-25 18:40:27 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
     connection, _ = server_socket.accept()
-    for _ in range(1):
+    for _ in range(2):
         connection.sendall(b"+PONG\r\n")
 
 if __name__ == "__main__":
