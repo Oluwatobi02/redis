de14ac98cf0bd3f27b456ee63607fbfcd28d53ff
Oluwatobi <tobiolajide01@gmail.com>
2024-11-25 21:20:42 -0500

    allow for concurrent connections with threading

--- app/main.py
+++ app/main.py
@@ -1,15 +1,22 @@
 import socket  # noqa: F401
+import threading
 
-
+def handle_connection(connection, address):
+    while connection.recv(8000):
+        connection.send(b"+PONG\r\n")
+        print("PONG")
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
-    # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    # server_socket.accept() # wait for client
+    
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
+    while True:
+        connection, address = server_socket.accept()
+        thread = threading.Thread(target=handle_connection, args=(connection, address))
+        thread.start()
 
 if __name__ == "__main__":
     main()
