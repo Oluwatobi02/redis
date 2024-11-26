c26a969da8004d59746551d1c4082b2f6575e1c8
Oluwatobi <tobiolajide01@gmail.com>
2024-11-25 20:47:45 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,8 +9,8 @@ def main():
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
 
-    connection, _ = server_socket.accept()
-    for _ in range(2):
+    while True:
+        connection, _ = server_socket.accept()
         print(connection.recv(1024).decode('utf-8'))
         
         connection.sendall(b"+PONG\r\n")
