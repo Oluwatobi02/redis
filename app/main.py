656fe9f429105bc33da24433757b69d0cba7f644
Oluwatobi <tobiolajide01@gmail.com>
2024-11-25 22:45:24 -0500

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 import socket  # noqa: F401
 import threading
 
-def handle_connection(connection, address):
-    while connection.recv(8000):
-        connection.send(b"+PONG\r\n")
-        print("PONG")
+def handle_connection(connection: socket.socket, address):
+    while connection:
+        data = connection.recv(8000)
+        if not data:
+            break
+        try:
+            message = data.decode("utf-8")
+            connection.sendall(b"+PONG\r\n")
+            print(f"Received: {message}")
+        except UnicodeDecodeError:
+            connection.sendall(b"-ERR invalid encoding\r\n")
+            continue
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -12,13 +20,12 @@ def main():
     # Uncomment this to pass the first stage
     
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    threads = []
+
     while True:
         connection, address = server_socket.accept()
-        t = threading.Thread(target=handle_connection, args=(connection, address))
-        t.start()
-        threads.append(t)
-    for t in threads:
-        t.join()
+        thread = threading.Thread(target=handle_connection, args=(connection, address))
+        thread.start()
+        thread.join()
+
 if __name__ == "__main__":
     main()
